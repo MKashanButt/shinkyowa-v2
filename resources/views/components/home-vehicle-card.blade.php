fabe7fd577e0a3ff780897258567384a917dcba1
MKashanButt <kashanbutt566@gmail.com>
2025-11-25 20:52:04 +0500

    add inquiry button on stock where fob is 0

--- resources/views/components/home-vehicle-card.blade.php
+++ resources/views/components/home-vehicle-card.blade.php
@@ -2,8 +2,8 @@
     <a href="/vehicle-info/{{ $id }}">
         <img src="{{ env('STOCK_IMG_LINK') . $img }}" alt="stock-image">
         <h4>{{ strtoupper($make) }} {{ strtoupper($model) }}</h4>
-        @if ($fob == 'Inquiry')
-            <button class="primary">{{ $fob }}</button>
+        @if ($fob == 0)
+            <button class="primary">Inquiry</button>
         @else
             <p>{{ $currency }} <span>{{ number_format($fob) }}</span></p>
         @endif
