05ef49e87f1e9c966ad956ba703a38d239455d04
MKashanButt <kashanbutt566@gmail.com>
2025-12-12 23:02:59 +0500

    fix customer account issue for now

--- app/Http/Controllers/ClientDashboardController.php
+++ app/Http/Controllers/ClientDashboardController.php
@@ -10,10 +10,49 @@ class ClientDashboardController extends Controller
 {
     public function index()
     {
-        $accountInfo = CustomerAccount::where('email', Auth::user()->email)
-            ->get()
-            ->first();
+        // $accountInfo = CustomerAccount::where('email', Auth::user()->email)
+        //     ->get()
+        //     ->first();
 
-        return view('client.dashboard', compact('accountInfo'));
+        $customerAccount = CustomerAccount::whereEmail(Auth::user()->email)->firstOrFail();
+
+        $id = $customerAccount->id;
+        $customerAccount->load([
+            'currency:id,symbol,code',
+            'country:id,name',
+            'agent:id,name',
+            'stock' => function ($query) use ($id) {
+                $query->with('make', 'currency', 'shipment', 'payment')
+                    ->where('customer_account_id', $id)
+                    ->orderBy('created_at', 'desc');
+            },
+            'payment' => function ($query) {
+                $query->with(['stock:id,sid'])
+                    ->orderBy('payment_date', 'desc')
+                    ->select([
+                        'id',
+                        'customer_account_id',
+                        'stock_id',
+                        'payment_date',
+                        'description',
+                        'amount',
+                        'in_yen',
+                        'payment_recieved_date',
+                        'status',
+                        'file',
+                    ]);
+            }
+        ]);
+
+        $customerAccount->buying = $customerAccount->stock->sum('fob');
+        $customerAccount->deposit = $customerAccount->payment->sum('amount');
+
+        $customerAccount->remaining_balance = $customerAccount->buying - $customerAccount->deposit;
+        $customerAccount->payment_count = $customerAccount->payment->count();
+        $customerAccount->reserved_stock_count = $customerAccount->stock->count();
+
+        return view('admin.customer-account.show', compact('customerAccount'));
+
+        // return view('client.dashboard', compact('accountInfo'));
     }
 }
