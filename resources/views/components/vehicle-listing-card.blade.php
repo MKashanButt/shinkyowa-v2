0a91a07061f252e04cda574b934ad048805675b4
MKashanButt <kashanbutt566@gmail.com>
2025-12-16 01:13:02 +0500

    add stock id and add limit on chassis

--- resources/views/components/vehicle-listing-card.blade.php
+++ resources/views/components/vehicle-listing-card.blade.php
@@ -4,7 +4,8 @@
         <div class="content">
             <h3>{{ strtoupper($year) }} {{ strtoupper($make) }} {{ strtoupper($model) }}</h3>
             <div class="text">
-                <p>Chassis: <span>{{ $chassis }}</span></p>
+                <p>Stock Id: <span>SKI-{{ str_pad($stockId, 2, 0, STR_PAD_LEFT) }}</span></p>
+                <p>Chassis: <span>{{ Str::limit($chassis, 6) }}</span></p>
                 <p>Year: <span>{{ $year }}</span></p>
                 <p>Mileage: <span>{{ $mileage }}</span></p>
                 <p>Doors: <span>{{ $doors }}</span></p>
