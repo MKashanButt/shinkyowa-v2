67815fae2a6b4281632f7d25f69c9f92717a45e9
Kashan Butt <kashanbutt566@gmail.com>
2026-01-10 21:41:53 +0500

    moved from stock_id dropdown to input field

--- resources/views/admin/payment/create.blade.php
+++ resources/views/admin/payment/create.blade.php
@@ -16,14 +16,12 @@
                     <div class="flex items-center gap-2">
                         <x-input-label for="stock_id" class="w-[32%] after:content-['*'] after:text-red-500">Stock
                             Id</x-input-label>
-                        <x-select-box id="stock_id" name="stock_id" class="w-4/5">
-                            <option value="" selected>Select Stock Id</option>
-                            @foreach ($stocks as $key => $item)
-                                <option value="{{ $key }}" {{ old('stock_id') == $key ? 'selected' : '' }}>
-                                    {{ 'SKI-' . $item }}
-                                </option>
-                            @endforeach
-                        </x-select-box>
+                        <div class="w-4/5 flex gap-1">
+                            <x-pill>SKI-</x-pill>
+                            <x-text-input type="text" id="stock_id" name="stock_id" class="flex-1"
+                                value="{{ old('stock_id') }}"
+                                placeholder="Enter Stock Id number with no preceding zero's" />
+                        </div>
                         <x-input-error :messages="$errors->get('stock_id')" class="mt-2" />
                     </div>
                     <div class="flex items-center gap-2">
