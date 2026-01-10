5140efdc1ef58cfe6b12f748b9c6a75451b9b034
Kashan Butt <kashanbutt566@gmail.com>
2026-01-10 21:41:14 +0500

    add pill component

---
+++ resources/views/components/pill.blade.php
@@ -0,0 +1,3 @@
+<span {{$attributes->merge([
+    'class' => 'inline-flex items-center rounded-sm bg-gray-400/10 px-2 py-1 text-xs font-medium text-gray-400 inset-ring inset-ring-gray-400/20'
+])}}>{{$slot}}</span>
