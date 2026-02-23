e9fb29c2df1b0370c39c0cbef014c26c0b143ee1
MKashanButt <kashanbutt566@gmail.com>
2026-02-24 01:59:46 +0500

    add stock id to show on vehicle cards

--- resources/views/components/home-vehicle-card.blade.php
+++ resources/views/components/home-vehicle-card.blade.php
@@ -1,11 +1,17 @@
+@props(['data'])
+
+{{-- @dd($data) --}}
+
 <div class="item">
-    <a href="/vehicle-info/{{ $id }}">
-        <img src="{{ env('STOCK_IMG_LINK') . $img }}" alt="stock-image" loading="lazy">
-        <h4>{{ strtoupper($make) }} {{ strtoupper($model) }}</h4>
-        @if ($fob == 0)
+    <a href="/vehicle-info/{{ $data['id'] }}">
+        <img src="{{ env('STOCK_IMG_LINK') . $data['thumbnail'] }}" alt="stock-image" loading="lazy">
+        <h4>{{ strtoupper($data['make']['name']) }} {{ strtoupper($data['model']) }}
+            <span class="pill">(SKI-{{ $data['sid'] }})</span>
+        </h4>
+        @if ($data['fob'] == 0)
             <button class="primary">Inquiry</button>
         @else
-            <p>{{ $currency }} <span>{{ number_format($fob) }}</span></p>
+            <p>{{ $data['currency']['symbol'] }} <span>{{ number_format($data['fob']) }}</span></p>
         @endif
     </a>
 </div>
