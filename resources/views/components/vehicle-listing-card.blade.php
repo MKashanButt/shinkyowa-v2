adc534e224797472917c38df7844895e7654a6e3
MKashanButt <kashanbutt566@gmail.com>
2026-03-04 22:49:13 +0500

    add fob on card

--- resources/views/components/vehicle-listing-card.blade.php
+++ resources/views/components/vehicle-listing-card.blade.php
@@ -22,6 +22,7 @@
             </div>
         </div>
         <div class="action">
+            <p>{{ $symbol }}{{ $fob }} {{ $code }}</p>
             <button>Get Quote</button>
         </div>
     </div>
