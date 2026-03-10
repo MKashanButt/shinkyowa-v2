537e54098a1af8979ddee186ad7e840e0f22fed2
MKashanButt <kashanbutt566@gmail.com>
2026-03-10 21:44:03 +0500

    update image checking

--- resources/views/admin/stock/edit.blade.php
+++ resources/views/admin/stock/edit.blade.php
@@ -52,16 +52,9 @@
                                     <div class="relative">
                                         <img src="{{ asset('storage/' . $image) }}"
                                             class="w-16 h-16 object-cover border rounded">
-                                        <label class="absolute top-0 right-0 bg-white rounded-full p-1 shadow">
+                                        <label class="w-100 h-100 absolute top-0 right-0 bg-white rounded-full p-1 shadow">
                                             <input type="checkbox" name="remove_images[]" value="{{ $image }}"
-                                                class="hidden peer">
-                                            <svg xmlns="http://www.w3.org/2000/svg"
-                                                class="h-4 w-4 text-red-500 peer-checked:block hidden" viewBox="0 0 20 20"
-                                                fill="currentColor">
-                                                <path fill-rule="evenodd"
-                                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
+                                                class="h-100 w-100">
                                         </label>
                                     </div>
                                 @endforeach
@@ -481,7 +474,7 @@
                     Array.from(e.target.files).forEach(file => {
                         const img = document.createElement('img');
                         img.src = URL.createObjectURL(file);
-                        img.className = 'w-16 h-16 object-cover border rounded';
+                        img.className = 'w-20 h-20 object-cover border rounded';
                         container.appendChild(img);
                     });
 
